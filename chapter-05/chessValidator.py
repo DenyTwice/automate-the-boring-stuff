0b229c3fd748a22c7156bd7707fab800c21bb0a2
LiquidFlare <ivinjoelabraham@gmail.com>
2023-01-14 16:09:40 +0530

    ADD : Add condition to check for invalid piece names

--- chapter-05/chessValidator.py
+++ chapter-05/chessValidator.py
@@ -4,10 +4,22 @@ def isValidChessBoard(dict):
     
     valid_rows = ['1', '2', '3', '4', '5', '6', '7', '8']
     valid_files = ['a', 'b',' c', 'd', 'e', 'f', 'g', 'h']
+    valid_color = ['w', 'b']
+    valid_pieces = ['pawn', 'rook', 'bishop', 'queen', 'king']
     chckr = list(itertools.product(valid_rows, valid_files))
+    color_cross_piece = list(itertools.product(valid_color, valid_pieces))
+    color_piece = []
     
-    w_count, w_pcount, b_count, b_pcount = 0, 0, 0, 0
+    for i in range(len(color_cross_piece)):
+        color_piece.append("".join(color_cross_piece[i]))
 
+    w_count, w_pcount, b_count, b_pcount = 0, 0, 0, 0
+    
+    for piece in dict.values():
+        if piece not in color_piece:
+            print(color_piece, piece)
+            return False
+    
     for key in dict:
         row_file = tuple(key)
         if row_file not in list(chckr):
